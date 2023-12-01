be15c83be7b01f7232127bc34afd6969e7fcbc97
home-fops9311 <fops9311@yandex.ru>
2023-12-01 17:15:16 +0300

    generate

--- data/jsonorder.go
+++ data/jsonorder.go
@@ -1,59 +1,67 @@
 package data
-//GENERATED json delivery data
-type RawDeliveryData struct{
-	Address string `json:"address"`
-	City string `json:"city"`
-	Zip string `json:"zip"`
-	Phone string `json:"phone"`
-	Name string `json:"name"`
-	Email string `json:"email"`
-	Region string `json:"region"`
-LocalID string `json:"-"`
-}
-//GENERATED json payment data
-type RawPaymentData struct{
-	CustomFee int `json:"custom_fee"`
-	Bank string `json:"bank"`
-	Amount int `json:"amount"`
-	Provider string `json:"provider"`
-	Currency string `json:"currency"`
-	Transaction string `json:"transaction"`
-	GoodsTotal int `json:"goods_total"`
-	DeliveryCost int `json:"delivery_cost"`
-	PaymentDt int `json:"payment_dt"`
-	RequestId string `json:"request_id"`
-LocalID string `json:"-"`
-}
+
+import "encoding/gob"
+
+func init() { gob.Register(RawOrderData{}) }
+
 //GENERATED json items data
-type RawItemsData struct{
-	Status int `json:"status"`
-	Sale int `json:"sale"`
-	Name string `json:"name"`
-	Rid string `json:"rid"`
-	Price int `json:"price"`
+type RawItemsData struct {
+	Price       int    `json:"price"`
+	ChrtId      int    `json:"chrt_id"`
+	Status      int    `json:"status"`
+	TotalPrice  int    `json:"total_price"`
+	Sale        int    `json:"sale"`
+	Name        string `json:"name"`
+	Rid         string `json:"rid"`
+	Size        string `json:"size"`
 	TrackNumber string `json:"track_number"`
-	Brand string `json:"brand"`
-	NmId int `json:"nm_id"`
-	TotalPrice int `json:"total_price"`
-	Size string `json:"size"`
-	ChrtId int `json:"chrt_id"`
-LocalID string `json:"-"`
+	Brand       string `json:"brand"`
+	NmId        int    `json:"nm_id"`
+	LocalID     string `json:"-"`
 }
-//GENERATED json Order data
-type RawOrderData struct{
-	CustomerId string `json:"customer_id"`
-	Entry string `json:"entry"`
-	OrderUid string `json:"order_uid"`
-	SmId int `json:"sm_id"`
-	InternalSignature string `json:"internal_signature"`
-	Items []RawItemsData `json:"items"`
-	Payment RawPaymentData `json:"payment"`
-	DeliveryService string `json:"delivery_service"`
-	Locale string `json:"locale"`
-	TrackNumber string `json:"track_number"`
-	OofShard string `json:"oof_shard"`
-	DateCreated string `json:"date_created"`
-	Shardkey string `json:"shardkey"`
-	Delivery RawDeliveryData `json:"delivery"`
-LocalID string `json:"-"`
+
+// GENERATED json delivery data
+type RawDeliveryData struct {
+	Phone   string `json:"phone"`
+	Name    string `json:"name"`
+	Email   string `json:"email"`
+	Region  string `json:"region"`
+	Address string `json:"address"`
+	City    string `json:"city"`
+	Zip     string `json:"zip"`
+	LocalID string `json:"-"`
+}
+
+// GENERATED json payment data
+type RawPaymentData struct {
+	DeliveryCost int    `json:"delivery_cost"`
+	Bank         string `json:"bank"`
+	PaymentDt    int    `json:"payment_dt"`
+	Provider     string `json:"provider"`
+	Currency     string `json:"currency"`
+	Transaction  string `json:"transaction"`
+	CustomFee    int    `json:"custom_fee"`
+	GoodsTotal   int    `json:"goods_total"`
+	Amount       int    `json:"amount"`
+	RequestId    string `json:"request_id"`
+	LocalID      string `json:"-"`
+}
+
+// GENERATED json Order data
+type RawOrderData struct {
+	OofShard          string          `json:"oof_shard"`
+	Payment           RawPaymentData  `json:"payment"`
+	DateCreated       string          `json:"date_created"`
+	DeliveryService   string          `json:"delivery_service"`
+	InternalSignature string          `json:"internal_signature"`
+	Locale            string          `json:"locale"`
+	Delivery          RawDeliveryData `json:"delivery"`
+	Entry             string          `json:"entry"`
+	CustomerId        string          `json:"customer_id"`
+	Items             []RawItemsData  `json:"items"`
+	OrderUid          string          `json:"order_uid"`
+	SmId              int             `json:"sm_id"`
+	Shardkey          string          `json:"shardkey"`
+	TrackNumber       string          `json:"track_number"`
+	LocalID           string          `json:"-"`
 }
